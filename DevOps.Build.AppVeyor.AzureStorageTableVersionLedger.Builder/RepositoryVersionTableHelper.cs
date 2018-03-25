ad82787ec2081751dcc5129db28973e8674f5314
Christopher Dorst <chris@dorst.email>
2018-03-25 16:50:54 -0500

    Update RepositoryVersionTableHelper.cs

--- DevOps.Build.AppVeyor.AzureStorageTableVersionLedger.Builder/RepositoryVersionTableHelper.cs
+++ DevOps.Build.AppVeyor.AzureStorageTableVersionLedger.Builder/RepositoryVersionTableHelper.cs
@@ -1,3 +1,6 @@
+// Copyright © Christopher Dorst. All rights reserved.
+// Licensed under the GNU General Public License, Version 3.0. See the LICENSE document in the repository root for license information.
+
 namespace DevOps.Build.AppVeyor.AzureStorageTableVersionLedger.Builder
 {
     /// <summary>Function returns an instance of RepositoryVersionTable</summary>
